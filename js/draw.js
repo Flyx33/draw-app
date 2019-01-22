3c87f222b3b18c246d698d245ac62abc356d2766
JP <jaronpate@gmail.com>
2019-01-21 18:39:00 -0600

    saving/polish/sidebar
    - Added different saving capabilities
    - Ceated some scaffoling for the sidebar
    - Polished some things

--- js/draw.js
+++ js/draw.js
@@ -11,12 +11,17 @@ window.addEventListener("resize", (e) => {
     load(JSON.stringify(strokeHistory));
 });
 
-// Find Inputs
+// Find Inputs/Elements
 var brushSlider = document.querySelector("#brushSlider"),
+undoButton = document.querySelector("#undoButton"),
 resetButton = document.querySelector("#resetButton"),
+saveButton = document.querySelector("#saveButton"),
+loadButton = document.querySelector("#loadButton"),
 colorButton = document.querySelector("#colorButton"),
 colorPicker = document.querySelector("#colorPicker");
-colorSamples = document.querySelectorAll(".colorSample");
+colorSamples = document.querySelectorAll(".colorSample"),
+sidebarButton = document.querySelector("#sidebarButton"),
+sidebar = document.querySelector(".sidebar");
 // Declare Variables
 var paint = false,
 brushColor = "#ffc0cb",
@@ -86,6 +91,7 @@ function draw(e){
 function load(data){
     var h = JSON.parse(data);
     h.forEach((e) => {
+        // Check if it is the end or begining of a stroke
         if(e.strokeEnd === true){
             ctx.closePath();
         }else if(e.strokeBegin === true){
@@ -105,20 +111,55 @@ function load(data){
 }
 
 function save(h){
+    // Turn temp history in a JSON string
     strokeData = JSON.stringify(h);
     return strokeData;
 }
 
 // === Options Functionality ===
-// Brush Size
+// Reset Brush Size
 brushSlider.addEventListener("dblclick", (e) => {
     brushSlider.value = 10;
 });
 
-// Reset
+// Reset Button
 resetButton.addEventListener("click", (e) => {
+    // Clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // Clear temp history
     strokeHistory = [];
+});
+
+// Undo Button
+undoButton.addEventListener("click", (e) => {
+    while(strokeHistory.length > 0){
+        // Check if it is the begining of a stroke
+        if(strokeHistory[strokeHistory.length - 1].strokeBegin){
+            // Remove begin stroke
+            strokeHistory.pop();
+            // And exit
+            break;
+        }else{
+            // If not begining of a stroke then remove a stroke data point
+            strokeHistory.pop();
+        }
+    }
+    // After exiting loop
+    // Redraw
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    load(JSON.stringify(strokeHistory));
+});
+
+// Save Button
+saveButton.addEventListener("click", (e) => {
+    localStorage.setItem("savedStrokes", save(strokeHistory));
+});
+
+// Load Button
+loadButton.addEventListener("click", (e) => {
+    // Make sure to check for party memebers on this when parties are implemented
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    load(localStorage.getItem("savedStrokes"));
 });
 
 // Color Picker
@@ -134,6 +175,28 @@ colorSamples.forEach((e) => {
     });
 });
 
+// Save Image Locally
+var localSaveBtn = document.querySelector('#localSaveButton');
+localSaveBtn.addEventListener("click", (e) => {
+    localSaveBtn.href = canvas.toDataURL();
+    localSaveBtn.download = "image.png";
+});
+
+// localSaveBtn.setAttribute('download', 'image.png');
+// localSaveBtn.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+
 // === History Functionality ===
 var strokeHistory = [];
 
+// === Sidebar Functionality ===
+sidebarButton.addEventListener("click", (e) => {
+    if(sidebar.style.width === "15em"){
+        sidebar.style.width = "20px";
+    }else{
+        sidebar.style.width = "15em";
+    }
+});
+
+canvas.addEventListener("mouseenter", (e) => {
+    sidebar.style.width = "20px";
+});
